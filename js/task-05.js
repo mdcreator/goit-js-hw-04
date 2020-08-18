19a9e6d1a5ed9aa6a53ee3e61f8bec1de3d0f7c4
Marina Domashenko <creator.marina@gmail.com>
2020-08-19 00:49:08 +0300

    Added task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -1,51 +1,48 @@
-bind для замены this в методах объекта
-Оформи вызов метода invokeInventoryAction таким образом, чтобы в качестве this для методов
+// bind для замены this в методах объекта
+// Оформи вызов метода invokeInventoryAction таким образом, чтобы в качестве this для методов
 
-inventory.add
-inventory.remove выступал объект inventory
+// inventory.add
+// inventory.remove выступал объект inventory
 
 const inventory = {
-    items: ['Knife', 'Gas mask'],
-    add(itemName) {
-        this.items.push(itemName);
-        return `Adding ${itemName} to inventory`;
-    },
-    remove(itemName) {
-        this.items = this.items.filter(item => item !== itemName);
-        return `Removing ${itemName} from inventory`;
-    },
+  items: ['Knife', 'Gas mask'],
+  add(itemName) {
+    this.items.push(itemName);
+    return `Adding ${itemName} to inventory`;
+  },
+  remove(itemName) {
+    this.items = this.items.filter(item => item !== itemName);
+    return `Removing ${itemName} from inventory`;
+  },
 };
 
 const invokeInventoryAction = function (itemName, action) {
-    const act = action(itemName);
-    const msg = `Invoking action on ${itemName}`;
-    return { act, msg };
+  const act = action(itemName);
+  const msg = `Invoking action on ${itemName}`;
+  return { act, msg };
 };
 
 const invokeAdd = invokeInventoryAction(
-    'Medkit',
-    inventory.add  // Write code in this line
+  'Medkit',
+  inventory.add.bind(inventory), // Write code in this line
 );
 const arrayAdd = [...inventory.items];
-/* 
-//console.log(invokeAdd);
+
+console.log(invokeAdd);
 //{ act: 'Adding Medkit to inventory', msg: 'Invoking action on Medkit' }
 
-//console.log(arrayAdd);
+console.log(arrayAdd);
 // ['Knife', 'Gas mask', 'Medkit']
-*/
 
 const invokeRemove = invokeInventoryAction(
-    'Gas mask',
-    inventory.remove  // Write code in this line
+  'Gas mask',
+  inventory.remove.bind(inventory), // Write code in this line
 );
 
 const arrayRemove = [...inventory.items];
 
-/*
-//console.log(invokeRemove);
+console.log(invokeRemove);
 //{ act: 'Removing Gas mask from inventory', msg: 'Invoking action on Gas mask' }
 
-//console.log(arrayRemove);
+console.log(arrayRemove);
 // ['Knife', 'Medkit']
-*/
